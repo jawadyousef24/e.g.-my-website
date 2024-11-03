e04f9ed6ce8b2658596f54d3880897ac69feb699
jawadyousef24 <hadiyousef770@gmail.com>
2024-11-03 16:28:16 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Welcome to My Website!');
+});
+<script src="script.js"></script>
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Welcome to My Website!');
+});
